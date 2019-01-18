7da729862616a4b7df82d4c38a4592ea35a36b0a
Paul Latzelsperger <paul@beardyinc.com>
2019-01-18 11:38:36 +0100

    added more tasks

--- src/com/digimindset/java/lesson1/StringOperations.java
+++ src/com/digimindset/java/lesson1/StringOperations.java
@@ -11,18 +11,30 @@ public class StringOperations {
   }
 
 
-  protected String concatWithStringFormat(String s1, String s2) {
+  private String concatWithStringFormat(String s1, String s2) {
     return String.format("%s %s", s1, s2);
   }
 
   // Hello world, replace e with X -> HXllo world
   public String replace(String input, String search, String replacer){
-    // ???
     return null;
   }
 
   // Hello world -> 11
   public int length(String input){
-    return -1; // ???
+    return -1;
   }
+
+  // 6 -> "6"
+  public String convertNumber(int number) {
+    return null;
+  }
+
+  // "Hello World", "o" -> 2
+  public int countOccurrence(String input, String search){
+    return -1;
+  }
+
+
+
 }
